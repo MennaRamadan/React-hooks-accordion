2a824c166dc343f829d7fc657267d02e8dc7bb85
mennatallah.ramadan <mennatallah.ramadan@itworx.com>
2020-07-20 14:38:49 +0200

    working with useSatehooks

--- src/Components/Accordion.js
+++ src/Components/Accordion.js
@@ -1,15 +1,26 @@
-import React from 'react';
+import React , {useState} from 'react';
 
 const Accordion = ({items})=> {
 
-    const renderedItems = items.map(item => {
+    //THIS NOT MEANS CREATING ARRAY, BUT ACCESS FIRST ELEMENT IN useSate IN ACTIVEINDEX AND SECOND ONE IN SETACTIVEINDEX
+    //FIRST ONEIS PIEACE OF STATE AND SECOND ONE IS FUNCTION TO UPDATE THIS PIEACE
+    const [activeIndex, setActiveIndex] = useState(null);
+
+    const onTitleClicked = (index) => {
+        setActiveIndex(index);
+    }
+
+    const renderedItems = items.map((item, index) => {
+
+        const active = index === activeIndex ? 'active' : '';
+        
         return(
             <React.Fragment key={item.title}>
-                <div className="title active">
+                <div className={`title ${active}`} onClick={ () => onTitleClicked(index)}>
                     <i className="dropdown icon"></i>
                     {item.title}
                 </div>
-                <div className="content active">
+                <div className={`content ${active}`}>
                     {item.content}
                 </div>
             </React.Fragment>
@@ -17,8 +28,10 @@ const Accordion = ({items})=> {
     })
 
     return(
-    <div className="ui styled accordion">{renderedItems}</div>
-    )
+        <div className="ui styled accordion">
+            {renderedItems}
+        </div>
+    );
 }
 
 export default Accordion;
