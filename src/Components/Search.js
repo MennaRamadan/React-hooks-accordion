9837caa439847cf1d68bdaff5764a555052176e4
mennatallah.ramadan <mennatallah.ramadan@itworx.com>
2020-07-27 14:52:35 +0200

    finishing search

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -19,17 +19,36 @@ const Search = () => {
             setResults(data.query.search);
         }
 
-        search();
+        if(term && !results.length){
+            search();
+        }else{
+            const timeoutId = setTimeout( () => {
+                if(term){
+                    search();
+                }
+            }, 1000);
+             //CLEANUP FUNCTION WILL INVOKED WHEN TERM CHANGES
+            return () => {
+                clearTimeout(timeoutId);
+            }
+        }
+        
     }, [term])
 
     const renderedResults = results.map( (result)=> {
         return(
             <div className="item" key={result.pageid}>
+                <div className="right floated content">
+                    <a className="ui button"
+                        href={`https://en.wikipedia.org?curid=${result.pageid}`}>
+                        Go
+                    </a>
+                </div>
                 <div className="content">
                     <div className="header">
                         {result.title}
                     </div>
-                    {result.snippet}
+                    <span dangerouslySetInnerHTML={{__html: result.snippet}}></span>
                 </div>
             </div>
         )
